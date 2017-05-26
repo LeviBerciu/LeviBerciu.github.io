a357a11de0a04a844425a548727eb495bd1789f5
lbeNemetos <lbe@nemetos.net>
2017-05-26 16:00:51 +0300

    c

---
+++ portfolio/vrView/js/vrView.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', onVrViewLoad)
+function onVrViewLoad() {
+  var vrView = new VRView.Player('#vrview', {
+    // image: 'https://storage.googleapis.com/vrview/examples/coral.jpg',
+    image: 'https://raw.githubusercontent.com/LeviBerciu/LeviBerciu.github.io/master/portfolio/vrView/img/coral.jpg',
+    is_stereo: true,
+    width: '100%',
+    height: '100%',
+    // is_autopan_off: true,
+    // is_vr_off: true,
+    // is_yaw_only: true,
+  });
+vrView.on('ready',function(){
+  vrView.addHotspot('hotspot-one', {
+    pitch: 0, // In degrees. Up is positive.
+    yaw: 0, // In degrees. To the right is positive.
+    radius: 0.5, // Radius of the circular target in meters.
+    distance: 5 // Distance of target from camera in meters.
+    });
+  });
+}
