03e6f5ef4f88650e4c7b441dc90d5a880a01e1f7
LeviBerciu <leviberciu.office@gmail.com>
2017-05-27 19:40:39 +0300

    c

--- portfolio/vrView/js/vrView.js
+++ portfolio/vrView/js/vrView.js
@@ -11,24 +11,24 @@ function onVrViewLoad() {
     // is_yaw_only: true,
   });
   vrView.on('ready',function(){
-    vrView.addHotspot('hotspot-one', {
-      pitch: 0, // In degrees. Up is positive.
-      yaw: 0, // In degrees. To the right is positive.
-      radius: 0.5, // Radius of the circular target in meters.
-      distance: 5 // Distance of target from camera in meters.
-      });
+    // vrView.addHotspot('hotspot-one', {
+    //   pitch: 0, // In degrees. Up is positive.
+    //   yaw: 0, // In degrees. To the right is positive.
+    //   radius: 0.5, // Radius of the circular target in meters.
+    //   distance: 5 // Distance of target from camera in meters.
+    //   });
     });
   vrView.on('click', function(event) {
-          if (event.id == 'hotspot-one') {
-          // Handle hotspot click.
-          vrView.setContent({
-            image: 'https://raw.githubusercontent.com/LeviBerciu/LeviBerciu.github.io/master/portfolio/vrView/img/360_1_v2.jpg',
-            // default_yaw: '200',
-            is_stereo: true,
-            width: '100%',
-            height: '100%',
-            is_autopan_off: true,
-        });
-      }
+      //     if (event.id == 'hotspot-one') {
+      //     // Handle hotspot click.
+      //     vrView.setContent({
+      //       image: 'https://raw.githubusercontent.com/LeviBerciu/LeviBerciu.github.io/master/portfolio/vrView/img/360_1_v2.jpg',
+      //       // default_yaw: '200',
+      //       is_stereo: true,
+      //       width: '100%',
+      //       height: '100%',
+      //       is_autopan_off: true,
+      //   });
+      // }
     });
 }
