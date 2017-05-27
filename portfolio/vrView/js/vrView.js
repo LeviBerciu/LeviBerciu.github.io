675203b0cb7ab4157dd926f1bf15d5db5452d85b
LeviBerciu <leviberciu.office@gmail.com>
2017-05-27 19:01:02 +0300

    c

--- portfolio/vrView/js/vrView.js
+++ portfolio/vrView/js/vrView.js
@@ -6,13 +6,13 @@ function onVrViewLoad() {
     is_stereo: true,
     width: '100%',
     height: '100%',
-    // is_autopan_off: true,
+    is_autopan_off: true,
     // is_vr_off: true,
     // is_yaw_only: true,
   });
   vrView.on('ready',function(){
     vrView.addHotspot('hotspot-one', {
-      pitch: 50, // In degrees. Up is positive.
+      pitch: 0, // In degrees. Up is positive.
       yaw: 0, // In degrees. To the right is positive.
       radius: 0.5, // Radius of the circular target in meters.
       distance: 5 // Distance of target from camera in meters.
@@ -27,6 +27,7 @@ function onVrViewLoad() {
             is_stereo: true,
             width: '100%',
             height: '100%',
+            is_autopan_off: true,
         });
       }
     });
