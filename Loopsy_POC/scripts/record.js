f32ddbaf2bdbc9a003d0c987fcfb8745a7c83f6d
LeviBerciu <leviberciu.office@gmail.com>
2020-05-12 12:12:48 +0300

    -

---
+++ Loopsy_POC/scripts/record.js
@@ -0,0 +1,30 @@
+var renderCanvas = document.getElementById('renderCanvas');
+var videoPreview = document.getElementById('videoPreview');
+var exportButton = document.getElementById('exportButton');
+
+exportButton.addEventListener('click', function(){
+    mediaRecorder.start();
+    setTimeout(function (){ mediaRecorder.stop(); }, 4000);
+});
+
+var options = {
+
+    videoBitsPerSecond : 2500000,
+    mimeType: 'video/webm;codecs=vp9',
+}
+
+
+var videoStream = renderCanvas.captureStream(30);
+var mediaRecorder = new MediaRecorder(videoStream, options);
+
+var chunks = [];
+mediaRecorder.ondataavailable = function(e) {
+    chunks.push(e.data);
+};
+
+mediaRecorder.onstop = function(e) {
+  var blob = new Blob(chunks, { 'type' : "video/webm" });
+  chunks = [];
+  var videoURL = URL.createObjectURL(blob);
+  videoPreview.src = videoURL;
+};
