8698b5fa8f0a8d25e64ad1ff59269cf8033516b6
LeviBerciu <leviberciu.office@gmail.com>
2020-05-29 11:21:56 +0300

    -

---
+++ Loopsyco/scripts/script.js
@@ -0,0 +1,21 @@
+// Navigation
+
+var navMenuButton = document.querySelector('.navMenuButton')
+var navCloseButton = document.querySelector('.navCloseButton')
+var navList = document.querySelector('.navList')
+var navUnderlay = document.querySelector('.navUnderlay')
+
+navMenuButton.addEventListener('click', function(){
+    navList.setAttribute('style', 'right: 0px');
+    navUnderlay.setAttribute('style', 'visibility: visible; opacity: 0,75');
+})
+
+navCloseButton.addEventListener('click', function(){
+    navUnderlay.setAttribute('style', 'visibility: hidden; opacity: 0');
+    navList.setAttribute('style', 'right: -200px');
+})
+
+navUnderlay.addEventListener('click', function(){
+    navUnderlay.setAttribute('style', 'visibility: hidden; opacity: 0');
+    navList.setAttribute('style', 'right: -200px');
+})
