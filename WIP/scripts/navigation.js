572001cb2e8da7e6e77995271e0343eb1cfd3488
LeviBerciu <leviberciu.office@gmail.com>
2022-11-02 19:28:10 +0200

    -

--- WIP/scripts/navigation.js
+++ WIP/scripts/navigation.js
@@ -1,14 +1,3 @@
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function () {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos || currentScrollPos <= 80) {
-//      document.getElementById('navbar').style.top = '0';
-//   } else {
-//      document.getElementById('navbar').style.top = '-81px';
-//    }
-//   prevScrollpos = currentScrollPos;
-// };
-
 var hamburgerCheckbox= document.getElementById("hamburger");
 var navTabs = document.getElementById("nav-tabs");
 
@@ -25,7 +14,6 @@ function showHideTabs() {
 const mediaQuery = window.matchMedia('(min-width: 960px)')
 function handleTabletChange(e) {
   if (e.matches) {
-    // console.log('Media Query Matched!')
     hamburgerCheckbox.checked = false;
     showHideTabs();
   }
