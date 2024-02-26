24ffc9f2b53eda397acbe8255faebbbb02e92187
LeviBerciu <leviberciu.office@gmail.com>
2024-02-26 11:34:03 +0200

    -

--- WIP/WhiskerBox/scripts/manager.js
+++ WIP/WhiskerBox/scripts/manager.js
@@ -8,19 +8,22 @@ let createScene = function() {
   // Textures
   const groundDiffuseTexture = new BABYLON.Texture("assets/ground_diffuse_texture.png",scene, false, false);
   const litterboxAmbientTexture = new BABYLON.Texture("assets/litterbox_ambient_texture.png",scene, false, false);
-  const maskAmbientTexture = new BABYLON.Texture("assets/mask_ambient_texture.png",scene, false, false);
+  const litterboxMaskAmbientTexture = new BABYLON.Texture("assets/litterbox_mask_ambient_texture.png",scene, false, false);
+  const litterboxMaskAlbedoTexture = new BABYLON.Texture("assets/litterbox_mask_albedo_texture.png",scene, false, false);
+  litterboxMaskAmbientTexture.coordinatesIndex = 1;
+  const emptyTexture = new BABYLON.Texture("assets/empty_texture.png",scene);
   const studioEnvironment = new BABYLON.CubeTexture("assets/studio.env");
 
   // Environment
   scene.environmentTexture = studioEnvironment;
+  //scene.environmentIntensity = 1.1;
   scene.clearColor = new BABYLON.Color4(0,0,0,0);
-  // scene.environmentIntensity = 0.5;
 
   // Camera
   const defaultCameraAlpha = 45;
   const defaultCameraBeta = 67.5;
   const defaultCameraRadius = 1.2;
-  const camera = new BABYLON.ArcRotateCamera("camera", BABYLON.Tools.ToRadians(defaultCameraAlpha), BABYLON.Tools.ToRadians(defaultCameraBeta), defaultCameraRadius, new BABYLON.Vector3(0, 0.26, 0));
+  const camera = new BABYLON.ArcRotateCamera("camera", BABYLON.Tools.ToRadians(defaultCameraAlpha), BABYLON.Tools.ToRadians(defaultCameraBeta), defaultCameraRadius, new BABYLON.Vector3(0, 0.25, 0));
   camera.attachControl(canvas, true);
   camera.wheelPrecision = 100;
   camera.panningSensibility = 0;
@@ -29,10 +32,6 @@ let createScene = function() {
   camera.upperRadiusLimit = 1.8;
   camera.upperBetaLimit = BABYLON.Tools.ToRadians(90);
 
-  // var light = new BABYLON.HemisphericLight("hemiLight", new BABYLON.Vector3(0, 1, 0), scene);
-  // light.intensity = 0.5;
-
-
   // Model
   BABYLON.SceneLoader.Append("assets/", "model.glb", scene, function(scene) {
     console.log(scene) // TBD
@@ -40,7 +39,7 @@ let createScene = function() {
     // Ground
     const groundMesh = scene.getMeshByName("ground")
     const groundMaterial = new BABYLON.StandardMaterial("groundMaterial",scene);
-    groundMaterial.diffuseTexture = groundDiffuseTexture
+    groundMaterial.diffuseTexture = groundDiffuseTexture;
     groundMaterial.diffuseTexture.hasAlpha = true;
     groundMaterial.useAlphaFromDiffuseTexture = true;
     groundMaterial.alpha = 0.5;
@@ -51,7 +50,7 @@ let createScene = function() {
     const litterboxTopMaterial = new BABYLON.PBRMaterial("litterboxTopMaterial", scene);
     litterboxTopMaterial.albedoColor = new BABYLON.Color3.FromHexString("#ffffff").toLinearSpace();
     litterboxTopMaterial.metallic = 0;
-    litterboxTopMaterial.roughness = 0.25;
+    litterboxTopMaterial.roughness = 0.2;
     litterboxTopMaterial.ambientTexture = litterboxAmbientTexture;
     litterboxTopMesh.material = litterboxTopMaterial;
 
@@ -60,38 +59,24 @@ let createScene = function() {
     const litterboxBottomMaterial = new BABYLON.PBRMaterial("litterboxBottomMaterial", scene);
     litterboxBottomMaterial.albedoColor = new BABYLON.Color3.FromHexString("#78909C").toLinearSpace();
     litterboxBottomMaterial.metallic = 0;
-    litterboxBottomMaterial.roughness = 0.25;
+    litterboxBottomMaterial.roughness = 0.2;
     litterboxBottomMaterial.ambientTexture = litterboxAmbientTexture;
     litterboxBottomMesh.material = litterboxBottomMaterial;
 
-    // Mask exterior
-    const maskExteriorMesh = scene.getMeshByName("mask_exterior");
-    const maskExteriorMaterial = new BABYLON.PBRMaterial("maskExteriorMaterial", scene);
-    maskExteriorMaterial.metallic = 0;
-    maskExteriorMaterial.roughness = 0.35;
-    maskExteriorMaterial.ambientTexture = maskAmbientTexture;
-    maskExteriorMesh.material = maskExteriorMaterial;
-
     // Mask interior
-    const maskInteriorMesh = scene.getMeshByName("mask_interior");
-    const maskInteriorMaterial = new BABYLON.PBRMaterial("maskInteriorMaterial", scene);
-    maskInteriorMaterial.metallic = 0;
-    maskInteriorMaterial.roughness = 0.35;
-    maskInteriorMaterial.ambientTexture = maskAmbientTexture;
-    maskInteriorMesh.material = maskInteriorMaterial;
+    const litterboxMaskMesh = scene.getMeshByName("litterbox_mask");
+    const litterboxMaskMaterial = new BABYLON.PBRMaterial("litterboxMaskMaterial", scene);
+    litterboxMaskMaterial.albedoTexture = litterboxMaskAlbedoTexture;
+    litterboxMaskMaterial.metallic = 0;
+    litterboxMaskMaterial.roughness = 0.4;
+    litterboxMaskMaterial.ambientTexture = litterboxMaskAmbientTexture;
+    litterboxMaskMesh.material = litterboxMaskMaterial;
 
-    // Selector
-    let selectedMaterial
-    let selectedPicker
+    // Colors
 
-    const exteriorPicker = document.getElementById("exteriorPicker");
-    exteriorPicker.addEventListener("click", function(event){
-      setSelected(maskExteriorMaterial, exteriorPicker);
-    });
-    
-    const interiorPicker = document.getElementById("interiorPicker");
-    interiorPicker.addEventListener("click", function(event){
-      setSelected(maskInteriorMaterial, interiorPicker);
+    const colorPicker = document.getElementById("colorPicker");
+    colorPicker.addEventListener("click", function(event){
+      colorPanel.setAttribute('class', 'visible');
     });
 
     const colorPanel = document.getElementById("colorPanel");
@@ -101,15 +86,6 @@ let createScene = function() {
       colorPanel.setAttribute('class', 'hidden');
     });
 
-    const colorPanelHeaderName = document.getElementById("colorPanelHeaderName");
-
-    function setSelected(material, picker){
-      colorPanel.setAttribute('class', 'visible');
-      selectedMaterial = material;
-      selectedPicker = picker;
-      colorPanelHeaderName.innerHTML = selectedPicker.dataset.name;
-    }
-
     const allSwatches = [];
     
     const swatches = document.getElementsByClassName("colorSwatch");
@@ -118,27 +94,20 @@ let createScene = function() {
         allSwatches.push(swatches[index]);
         swatches[index].style.backgroundColor = swatches[index].dataset.color;
         swatches[index].addEventListener("click", function(){
-          selectedMaterial.albedoColor = new BABYLON.Color3.FromHexString(swatches[index].dataset.color).toLinearSpace();
-          selectedPicker.style.backgroundColor = swatches[index].dataset.color;
-          selectedPicker.dataset.contrast = swatches[index].dataset.contrast;
-          selectedPicker.querySelector(".swatchName").innerHTML = swatches[index].querySelector(".swatchName").innerHTML
-          selectedPicker.querySelector(".swatchCode").innerHTML = swatches[index].querySelector(".swatchCode").innerHTML
+          litterboxMaskMaterial.albedoTexture = emptyTexture;
+          litterboxMaskMaterial.albedoColor = new BABYLON.Color3.FromHexString(swatches[index].dataset.color).toLinearSpace();
+          colorPicker.style.backgroundColor = swatches[index].dataset.color;
+          colorPicker.dataset.contrast = swatches[index].dataset.contrast;
+          colorPicker.querySelector(".swatchName").innerHTML = swatches[index].querySelector(".swatchName").innerHTML
+          colorPicker.querySelector(".swatchCode").innerHTML = swatches[index].querySelector(".swatchCode").innerHTML
         });
       })(i);
     };
 
-    // Random 
-    let randomSwatch
+    // Random Color Assigner
     
-    randomSwatch = allSwatches[Math.floor(Math.random()*allSwatches.length)];
-    selectedMaterial = maskExteriorMaterial;
-    selectedPicker = exteriorPicker;
-    randomSwatch.click() ;
-    
-    randomSwatch = allSwatches[Math.floor(Math.random()*allSwatches.length)];
-    selectedMaterial = maskInteriorMaterial;
-    selectedPicker = interiorPicker;
-    randomSwatch.click() ;
+    let randomSwatch = allSwatches[Math.floor(Math.random()*allSwatches.length)];
+    //randomSwatch.click() ;
 
   })
 
