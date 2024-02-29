04e6da1c91d8e36ccedf9dfd24cf7bcf35ba8994
LeviBerciu <leviberciu.office@gmail.com>
2024-02-29 15:16:50 +0200

    -

--- WIP/WhiskerBox/scripts/manager.js
+++ WIP/WhiskerBox/scripts/manager.js
@@ -243,3 +243,15 @@ x.addEventListener("change", function() {
 
 
 
+const switchTheme = () => {
+  const rootElement = document.documentElement;
+  let dataTheme = rootElement.getAttribute("data-theme");
+  let newTheme = (dataTheme === "light") ? "dark" : "light";
+  rootElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme)
+};
+
+document.getElementById("themeSwitcher").addEventListener("click", switchTheme)
+
+
+
